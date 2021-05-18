1761c07072808bb1d2e091409f55125581de100e
Geek Hub <robergeekhub@gmail.com>
2021-05-18 22:05:40 +0200

    añadir el crud en el controlador

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,9 +1,23 @@
 const Usuario = require("../models/usuario");
 
-class Usuario {
+class usuarioController {
 
     constructor() {
+    }
+    async indexAll() {
+        return Usuario.find().limit(10);
+    }
 
+    async store(usuario) {
+        return Usuario.create(usuario);
+    }
+
+    async update(id, usuario) {    
+        return Usuario.findByIdAndUpdate(id,usuario);
+    }
+
+    async destroy(id) {
+        return Usuario.findByIdAndRemove(id);
     }
 }
 
